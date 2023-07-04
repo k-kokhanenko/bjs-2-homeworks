078436c8e25827c77ff05f80976aa1490de6a526
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-07-04 17:26:41 +0300

    ПРавки в addMarks

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,10 +11,7 @@ Student.prototype.setSubject = function (subjectName) {
 
 Student.prototype.addMarks = function (...marks) {
     if (this.hasOwnProperty("marks")) {
-        let length = this.marks.length;
-        for (let i = 0; i < marks.length; i++) {
-            this.marks[length + i] = marks[i];
-        }
+        this.marks.push(...marks);
     }
 }
 
