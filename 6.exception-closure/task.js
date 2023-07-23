d6c073ca888d65d831528e5ef68d19b64e90636a
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-07-23 18:18:47 +0300

    initial commit

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,46 @@
-﻿
+﻿function parseCount(value) {
+    const result = parseFloat(value);
+
+    if (Number.isNaN(result)) 
+        throw new Error("Невалидное значение");
+    
+    return result;
+}
+
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } catch (error) {
+        return error;        
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || a + c < b || b + c < a)
+            throw new Error("Треугольник с такими сторонами не существует");
+
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const p = this.perimeter / 2;
+        return Math.sqrt(p * (p-a) * (p-b) * (p-c)).toFixed(3); 
+    }
+}
+
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return error;
+    }
+    
+}
