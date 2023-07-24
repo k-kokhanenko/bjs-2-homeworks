7f4127d39fbefb87c9743af03f56fc02100cddbc
k-kokhanenko <k.kokhanenko@yandex.ru>
2023-07-24 20:54:04 +0300

    last commit

--- 7.async/task.js
+++ 7.async/task.js
@@ -5,13 +5,13 @@ class AlarmClock {
     }
 
     addClock(time, callback) {
-        if (time === undefined) {
+        if (time === undefined || time == null) {
             throw new Error("Отсутствуют обязательные аргументы");
         }
-
+    
         if (callback === undefined) {
             throw new Error("Отсутствуют обязательные аргументы");
-        }        
+        }       
 
         // Проверьте, есть ли звонок с таким же временем. Если есть, выведите предупреждение в консоль с помощью console.warn('Уже присутствует звонок на это же время').
         let result = this.alarmCollection.find(item => item.time == time);
